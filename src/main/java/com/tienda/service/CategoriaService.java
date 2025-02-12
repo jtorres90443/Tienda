703938fa349b91b78d22c96cb63dd480b99a96c1
jtorres90443 <jtorres90443@ufide.ac.cr>
2025-02-11 23:55:13 -0600

    Semana 05

---
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -0,0 +1,8 @@
+package com.tienda.service;
+import java.util.List;
+import com.tienda.domain.Categoria;
+
+public interface CategoriaService {
+    
+    public List<Categoria> getCategorias(boolean activos);
+}
