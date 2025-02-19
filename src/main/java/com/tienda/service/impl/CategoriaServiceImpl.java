952c16651c3d5449c77440e2836e167a677c34ab
jtorres90443 <jtorres90443@ufide.ac.cr>
2025-02-18 22:51:14 -0600

    Semana 06

--- src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
+++ src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
@@ -1,4 +1,5 @@
 package com.tienda.service.impl;
+
 import com.tienda.dao.CategoriaDao;
 import com.tienda.domain.Categoria;
 import com.tienda.service.CategoriaService;
@@ -8,19 +9,37 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class CategoriaServiceImpl implements CategoriaService{
-    
+public class CategoriaServiceImpl implements CategoriaService {
+
     @Autowired
     private CategoriaDao categoriaDao;
-    
-    public List<Categoria> getCategorias(boolean activos){
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Categoria> getCategorias(boolean activos) {
         var lista = categoriaDao.findAll();
-        
         if (activos) {
-            lista.removeIf(c -> !c.isActivo());
+            lista.removeIf(e -> !e.isActivo());
         }
-        System.out.println("Lista filtrada (activos=" + activos + "): " + lista);
         return lista;
     }
 
+    @Override
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaDao.delete(categoria);
+    }
 }
+
